c3d1d72b218eba54355a5e02759665afdce827c8
Shaurya <Shaurya@Shauryas-MacBook-Pro.local>
2016-09-08 02:03:38 +0530

    minor changes

--- main/views.py
+++ main/views.py
@@ -25,7 +25,7 @@ def yt_grabber(vid):
 
 def post_msg(fbid, data):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'% access_token
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"attachment":{"type":"audio", "payload":{"url":data}}}})
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"attachment":{"type":"file", "payload":{"url":data}}}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
